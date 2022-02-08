70d2cfd4163dc4220b254539b39495e8770fe6fc
Jakub Soboczyński <teziovsky@gmail.com>
2022-02-08 21:16:23 +0100

    Updated main file

--- src/index.js
+++ src/index.js
@@ -1,25 +1,43 @@
 #! /usr/bin/env node
 
-const { checkFileExist, getGithubData } = require("./utils/utils");
+const {
+  checkFileExist,
+  getGithubData,
+  saveMarkdownFile,
+  saveJsonFile,
+  wait,
+  preparePackageJSON,
+} = require("./utils/utils");
 const getQuestions = require("./utils/questions");
 const generateMarkdown = require("./utils/markdown");
 const inquirer = require("inquirer");
+const { exec } = require("shelljs");
 
+const gitUrl = exec("git remote get-url --all origin", {
+  silent: true,
+}).replace("\n", "");
 const packageJsonPath = process.cwd() + "/package.json";
 
 async function init() {
-  const exist = checkFileExist(packageJsonPath);
+  const packageJsonExist = checkFileExist(packageJsonPath);
 
-  if (exist) {
+  if (packageJsonExist) {
     const packageJSON = await require(packageJsonPath);
 
-    const questions = await getQuestions(packageJSON);
+    const questions = await getQuestions(packageJSON, gitUrl);
     const responses = await inquirer.prompt(questions);
 
-    const githubData = await getGithubData(responses);
-    const README = await generateMarkdown("fucking markdown");
+    const githubData = await getGithubData(gitUrl);
+    const README = await generateMarkdown(responses, githubData);
 
-    console.log("README: ", README);
+    await wait(1000);
+    await saveMarkdownFile(README, "README");
+
+    if (responses.update_package_json === "Yes") {
+      await wait(1000);
+      const newPackageJSON = preparePackageJSON(packageJSON, responses);
+      await saveJsonFile(newPackageJSON, "package");
+    }
   } else {
     console.log(
       "The package.json does not exist in the current directory. Run command in the root directory of your project!"
