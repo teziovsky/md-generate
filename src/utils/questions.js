38eaaff22f2c9ca4fd95678b54ad17b08738b0da
Jakub Soboczyński <teziovsky@gmail.com>
2022-08-11 22:24:30 +0200

    feat: update node package question

--- src/utils/questions.js
+++ src/utils/questions.js
@@ -83,7 +83,7 @@ function getQuestions(packageJSON, gitUrl) {
       default() {
         return packageJSON?.engines?.node ? packageJSON.engines.node : ">=v14.0.0";
       },
-      choices: [">=v12.0.0", ">=v14.0.0", ">=v16.0.0", ">=v17.0.0"],
+      choices: [">=v12.0.0", ">=v14.0.0", ">=v16.0.0", ">=v18.0.0"],
     },
     {
       name: "npm_version",
